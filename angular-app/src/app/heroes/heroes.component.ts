5a1b3132d027aac37835b477ef14cd1814ae3a2c
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2022-01-14 14:49:44 +0200

    Add Services 50% done

--- angular-app/src/app/heroes/heroes.component.ts
+++ angular-app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../seed-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +11,21 @@ import { HEROES } from '../seed-heroes';
 
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
-  onSelect(hero: Hero): void{
+  heroes: Hero[] = [];
+
+  onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
-  heroes = HEROES;
-  constructor() { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-
+    this.getHeroes();
   }
 
 }
