d3ff1aa1e57a18ba3c0df448f362cf68ab93c1d0
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2022-01-15 18:29:55 +0200

    Navigation Chapter complete

--- angular-app/src/app/hero.service.ts
+++ angular-app/src/app/hero.service.ts
@@ -15,6 +15,14 @@ export class HeroService {
     this.messageService.add("Hero service: Fetched all heroes")
     return heroes;
   }
-  constructor(private messageService : MessageService) { }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(x => x.id == id)!;
+    this.messageService.add(`Hero service: fetched hero id=${id}`)
+    return of(hero);
+  }
+  constructor(private messageService: MessageService) { }
 }
 
--- angular-app/src/app/heroes/heroes.component.ts
+++ angular-app/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 
 @Component({
@@ -11,18 +10,12 @@ import { MessageService } from '../message.service';
 })
 
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService : MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`Heroes component: Selected hero id is ${hero.id}`)
   }
 
   getHeroes(): void {
