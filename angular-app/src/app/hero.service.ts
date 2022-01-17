faf1f0679e8caa38845cbea638028a40462bc57b
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2022-01-17 15:36:35 +0200

    Chapter 6 get data from server mock server done

--- angular-app/src/app/hero.service.ts
+++ angular-app/src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './seed-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,25 @@ import { MessageService } from './message.service';
 
 
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add("Hero service: Fetched all heroes")
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(x => x.id == id)!;
-    this.messageService.add(`Hero service: fetched hero id=${id}`)
+    this.log(`Hero service: fetched hero id=${id}`)
     return of(hero);
   }
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 }
 
