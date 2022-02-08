cee0fc107ad765178eb3dbd3b47dda3ef670f245
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2022-02-08 14:30:31 +0200

    added statehandling

--- team-generator/src/app/app.component.ts
+++ team-generator/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'team-generator';
+  newMemberInput = ""
+  membersArray: string[] = [];
+
+  onInput(member:string){
+    this.newMemberInput = member
+  }
+
+  addToMembers = () => {
+    this.membersArray.push(this.newMemberInput)
+    console.log(this.membersArray)
+  }
 }
+
